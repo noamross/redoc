346e481b89dea373731cb0f99256d8128e7664db
Noam Ross <noam.ross@gmail.com>
2018-12-11 10:01:05 -0500

    Add infrastructure for pre-processor

--- R/utils.R
+++ R/utils.R
@@ -15,3 +15,24 @@ escape_captures <- function(str) {
 readcsv <- function(x) {
   utils::read.csv(x, stringsAsFactors = FALSE)
 }
+
+readfile <- function(x) {
+  readChar(x, file.info(x)$size)
+}
+
+file_with_meta_ext <- function(file, meta_ext, ext = tools::file_ext(file)) {
+  paste(tools::file_path_sans_ext(file),
+        ".", meta_ext, ".", ext, sep = "")
+}
+
+file_with_ext <- function(file, ext) {
+  paste(tools::file_path_sans_ext(file), ".", ext, sep = "")
+}
+
+get_parent_env_with <- function(var_name) {
+  for (frame in rev(sys.frames())[-1]) {
+    if (exists(var_name, envir = frame, inherits = FALSE))
+      return(frame)
+  }
+  stop("No parent environment found with \"", var_name, "\"")
+}
