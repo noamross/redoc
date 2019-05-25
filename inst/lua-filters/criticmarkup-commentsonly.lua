26cd6369124a7b82c51d1241449dab5197ed4b58
Noam Ross (Travis-CI) <noam.ross@gmail.com>
2019-05-25 16:08:08 -0400

    Do not add spaces around CM syntax when dedoc'ing

--- inst/lua-filters/criticmarkup-commentsonly.lua
+++ inst/lua-filters/criticmarkup-commentsonly.lua
@@ -9,17 +9,17 @@ return {
         returnval = {}
       elseif elem.classes[1] and elem.classes[1] == "comment-start" then
         commentholder[elem.attributes["id"]] = elem
-        returnval = pandoc.RawInline(FORMAT, "{== ")
+        returnval = pandoc.RawInline(FORMAT, "{==")
       elseif elem.classes[1] and elem.classes[1] == "comment-end" then
         local comment = commentholder[elem.attributes["id"]]
         if comment == nil then
           returnval = pandoc.RawInline(FORMAT, "")
         elseif #comment.content == 0 then
-          returnval = pandoc.RawInline(FORMAT, " ==}")
+          returnval = pandoc.RawInline(FORMAT, "==}")
         else
-          returnval = { pandoc.RawInline(FORMAT, " ==}{>> ") } ..
+          returnval = { pandoc.RawInline(FORMAT, "==}{>>") } ..
             comment.content ..
-            { pandoc.RawInline(FORMAT, " <<}") }
+            { pandoc.RawInline(FORMAT, "<<}") }
         end
       else
         returnval = nil
--- inst/lua-filters/criticmarkup.lua
+++ inst/lua-filters/criticmarkup.lua
@@ -4,26 +4,26 @@ return {
     Span = function (elem)
       local returnval
       if elem.classes[1] and elem.classes[1] == "insertion" then
-        local opener = { pandoc.RawInline(FORMAT, "{++ ") }
-        local closer = { pandoc.RawInline(FORMAT, " ++}") }
+        local opener = { pandoc.RawInline(FORMAT, "{++") }
+        local closer = { pandoc.RawInline(FORMAT, "++}") }
         returnval = opener .. elem.content .. closer
       elseif elem.classes[1] and elem.classes[1] == "deletion" then
-        local opener = { pandoc.RawInline(FORMAT, "{-- ") }
-        local closer = { pandoc.RawInline(FORMAT, " --}") }
+        local opener = { pandoc.RawInline(FORMAT, "{--") }
+        local closer = { pandoc.RawInline(FORMAT, "--}") }
         returnval = opener .. elem.content .. closer
       elseif elem.classes[1] and elem.classes[1] == "comment-start" then
         commentholder[elem.attributes["id"]] = elem
-        returnval = pandoc.RawInline(FORMAT, "{== ")
+        returnval = pandoc.RawInline(FORMAT, "{==")
       elseif elem.classes[1] and elem.classes[1] == "comment-end" then
         local comment = commentholder[elem.attributes["id"]]
         if comment == nil then
           returnval = pandoc.RawInline(FORMAT, "")
         elseif #comment.content == 0 then
-          returnval = pandoc.RawInline(FORMAT, " ==}")
+          returnval = pandoc.RawInline(FORMAT, "==}")
         else
-          returnval = { pandoc.RawInline(FORMAT, " ==}{>> ") } ..
+          returnval = { pandoc.RawInline(FORMAT, "==}{>>") } ..
             comment.content ..
-            { pandoc.RawInline(FORMAT, " <<}") }
+            { pandoc.RawInline(FORMAT, "<<}") }
         end
       else
         returnval = nil
